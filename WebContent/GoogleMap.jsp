e847a9ac1fce4f88b0b62d3c2baf12b4a833fd7c
EomJaeWoong <djawodnd6535@naver.com>
2014-11-20 21:24:19 +0900

    지도 클릭시 마커 생성 기능 추가

--- WebContent/GoogleMap.jsp
+++ WebContent/GoogleMap.jsp
@@ -14,9 +14,14 @@
 	key=AIzaSyBEJKMd2ye82eIix9TBpMo9ZzOWcfNgFoo&sensor=TRUE">
   </script>
   <script type="text/javascript" src="./map/googlemaps.js"></script>
-	<title>Insert title here</title>
+	<title>Google Map!?</title>
 </head>
 <body onload="initialize()">
-	<div id="map"></div>
+	<div id="search_bar">
+		<input type="text" name="search">
+		<input type="submit" value="search!">
+	</div>
+	<div id="map">
+	</div>
 </body>
 </html>
--- WebContent/map/googlemaps.js
+++ WebContent/map/googlemaps.js
@@ -1,9 +1,36 @@
+var markersArray = [];
+var map;
+var goldStar = {
+		  path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+		  fillColor: "yellow",
+		  fillOpacity: 0.8,
+		  scale: 1,
+		  strokeColor: "gold",
+		  strokeWeight: 14
+		};
+
 function initialize() {
+		var myLating = new google.maps.LatLng(-25.363882,131.044922);
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+          center: myLating,
+          zoom: 2,
+          mapTypeId: google.maps.MapTypeId.TERRAIN
         };
-        var map = new google.maps.Map(document.getElementById("map"),
+        map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
+        
+        google.maps.event.addListener(map, 'click', function(event){
+        	addMarker(event.latLng);
+        });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function addMarker(location){
+	marker = new google.maps.Marker({
+		position: location,
+		map: map
+	});
+	markersArray.push(marker);
+	marker.setMap(map);
 }
